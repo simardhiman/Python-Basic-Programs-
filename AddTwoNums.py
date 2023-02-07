12584a9cce880a348491dd8244ba460d73efb587
Simar Dhiman <itssimardhiman@gmail.com>
2023-02-07 19:19:27 +0530

    Add files via upload

---
+++ AddTwoNums.py
@@ -0,0 +1,15 @@
+# pre-defined Variables
+
+#num1 = 30
+#num2 = 40
+#sum = num1+num2
+#print ("the sum of given two number is ", sum)
+
+# user-inputs
+
+num1 = float(input("enter a number here: "))
+num2 = float(input("enter another number: "))
+
+sum = num1 + num2
+
+print("the sum of the provided two numbers is", sum)
