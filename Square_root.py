aae81139bcbb0cd75d42c99defa6ae5843b5ec7e
Simar Dhiman <itssimardhiman@gmail.com>
2023-02-07 19:37:25 +0530

    Add files via upload

---
+++ Square_root.py
@@ -0,0 +1,13 @@
+#Using Exponentiation
+
+#num = 64
+#num1 = int(input("Enter a number here: "))
+#sr = num**(0.5)
+#print("The square root of the given number is",sr)
+
+#Using Math Module
+
+import math
+num = int(input("Enter a number here: "))
+sr = math.sqrt(num)
+print("The square root of the given number is",sr)
